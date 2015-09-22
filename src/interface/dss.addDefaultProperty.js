0010c2cc662b6f23163477fbda06639d7460c38f
Anderson Aguiar <revsoft10@gmail.com>
2015-09-22 19:04:26 -0300

    Organize interface

--- src/interface/dss.addDefaultProperty.js
+++ src/interface/dss.addDefaultProperty.js
@@ -1,33 +1,35 @@
 //File : src/interface/dss.addDefaultProperty.js
 
-(function(dss){
-'use strict';
+(function(dss) {
+  'use strict';
 
 	function capitalizeFirstLetter(string) {
-	    return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 
-	dss.core.defineMethod('addDefaultPropery',function(nameSpace,defaultProperty){
+	dss.core.defineMethod('addDefaultPropery', function(nameSpace, defaultProperty) {
 		defaultProperty = defaultProperty();
-		defaultProperty.context.addEventListener(defaultProperty.event,
-			function dssDefaultPropertyEventHandler(){
+		defaultProperty.context.addEventListener(
+      defaultProperty.event,
+			function dssDefaultPropertyEventHandler() {
 				var properties = defaultProperty.getter.apply(defaultProperty,arguments);
-				for(var prop in properties){
-					dss.setProperty(nameSpace+capitalizeFirstLetter(prop),properties[prop]);
+				for(var prop in properties) {
+					dss.setProperty(nameSpace + capitalizeFirstLetter(prop), properties[prop]);
 				}
-			});
+			}
+    );
 	});
 
-	dss.core.defineMethod('addDynamicPropery',function(nameSpace,defaultProperty){
+	dss.core.defineMethod('addDynamicPropery', function(nameSpace, defaultProperty) {
 		defaultProperty = defaultProperty();
-		defaultProperty.context.addEventListener(defaultProperty.event,
-			function dssDefaultPropertyEventHandler(){
-				var properties = defaultProperty.getter.apply(defaultProperty,arguments);
-				for(var prop in properties){
-					dss.setProperty(nameSpace+capitalizeFirstLetter(prop),properties[prop]);
+		defaultProperty.context.addEventListener(
+      defaultProperty.event,
+			function dssDefaultPropertyEventHandler() {
+				var properties = defaultProperty.getter.apply(defaultProperty, arguments);
+				for(var prop in properties) {
+					dss.setProperty(nameSpace + capitalizeFirstLetter(prop), properties[prop]);
 				}
-			});
+			}
+    );
 	});
-
 })(this.dss);
-
--- src/interface/dss.setProperty.js
+++ src/interface/dss.setProperty.js
@@ -1,15 +1,16 @@
 //File : src/interface/dss.setProperty.js
 
-(function(dss){
-'use strict';
+(function(dss) {
+  'use strict';
 
-	dss.core.defineMethod('setProperty',function(property,value){
-		if (!dss.core.dynamics)
+	dss.core.defineMethod('setProperty',function(property, value) {
+		if (!dss.core.dynamics) {
 			dss.core.dynamics = {};
+    }
+
 		dss.core.dynamics[property] = value;
-		if (dss.core.IS_INITIALIZED){
+		if (dss.core.IS_INITIALIZED) {
 			dss.core.refreshDss(dss.core.refreshValues);
 		}
 	});
-
 })(this.dss);
