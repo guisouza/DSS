48829847e86496d27aed47fe4c337b8654a26638
Felipe Amaral <feliperamaral80@gmail.com>
2015-09-23 09:23:14 -0300

    Optimized core.render

--- src/core/dss.core.render.js
+++ src/core/dss.core.render.js
@@ -3,15 +3,19 @@
 (function(dss){
 'use strict';
 	dss.core.render = function (){
-		var styles = document.querySelectorAll('[rel="dss-generated-stylesheet"]');
-		[].forEach.call(styles,function(style){
-			document.head.removeChild(style);
-		});
+
 		var style = document.createElement("style");
 		style.setAttribute('rel','dss-generated-stylesheet');
-		style.appendChild(document.createTextNode(dss.core.generateCss(dss.core.flatRules())));
 		document.head.appendChild(style);
+
+		dss.core.render = function(){
+
+			while(style.firstChild){
+				style.removeChild(style.firstChild);
+			}
+
+			style.appendChild(document.createTextNode(dss.core.generateCss(dss.core.flatRules())));
+		};
 	};
 })(this.dss);
-
 
