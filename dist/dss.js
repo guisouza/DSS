3f4ac6895ec10f17a10dc410155afc3d889935b3
João Neto <joaopintoneto@gmail.com>
2015-09-23 22:07:02 -0300

    chore(dss.core): build correct dist

--- dist/dss.js
+++ dist/dss.js
@@ -388,10 +388,9 @@
 'use strict';
 
 	dss.core.parseRule = function(rule){
-        var pattern = /(.*){([^}]*)}/gmi;
-        var x = pattern.exec(rule);
-        dss.core.findDynamics(x[1],x[2]);
-        // console.log(rule);
+		var pattern = /(.*){([^}]*)}/gmi;
+		var x = pattern.exec(rule);
+		dss.core.findDynamics(x[1],x[2]);
 	};
 
 })(this.dss);
